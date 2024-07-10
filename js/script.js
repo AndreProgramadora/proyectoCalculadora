4683fa3b5c36c5fd77e3cca859d9aeb16fc72a67
programadora <andrius.data@gmail.com>
2024-07-10 11:33:48 +0200

    comic1

---
+++ js/script.js
@@ -0,0 +1,18 @@
+
+function addTextColores(color, texto){
+    document.write("<p style=color:" + color +">Hola soy yo</p>")
+}
+
+addTextColores("red");
+
+function addTextColores2(color, texto){
+    document.write("<p style=color:" + color +">"+ texto +"</p>")
+}
+
+addTextColores2("blue", "segundo ejemplo");
+
+function escribir(color, text){
+    document.write("<h2 style=color:"+ color + ">"+ text +"</h2>")
+}
+
+escribir("purple", "Hoola soy Andrea")
